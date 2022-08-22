b625e215167fe1eaa1bd56431788988b1e4ca47f
Jake Young <jake_young@dell.com>
2022-08-22 23:27:26 +0100

    Adding the feature and What is GPT-3

--- src/components/brand/imports.js
+++ src/components/brand/imports.js
@@ -4,10 +4,10 @@ import atlassian from '../../assets/atlassian.png';
 import dropbox from '../../assets/dropbox.png';
 import shopify from '../../assets/shopify.png';
 
-export{
-    google,
-    slack,
-    atlassian,
-    dropbox,
-    shopify
-}
+export {
+  google,
+  slack,
+  atlassian,
+  dropbox,
+  shopify,
+};
