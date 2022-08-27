aa3f61516a900cb1648c1e65dcddc108e50d56ea
Jake Young <jake_young@dell.com>
2022-08-27 17:08:22 +0100

    Adding CTA

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 export { default as Article } from './Article/article';
 export { default as Brand } from './brand/Brand';
-export { default as CTA } from './Cta/cta';
+export { default as CTA } from './CTA/CTA';
 export { default as Feature } from './feature/Feature';
 export { default as Navbar } from './navbar/Navbar';
