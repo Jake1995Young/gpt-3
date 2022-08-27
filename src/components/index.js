9005b52d95c7692c88d1de97285c98ae5560af89
Jake Young <jake_young@dell.com>
2022-08-27 16:49:37 +0100

    Adding possibility and features

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
-export { default as Article } from './article/article';
-export { default as Brand } from './brand/brand';
-export { default as CTA } from './cta/cta';
-export { default as Feature } from './feature/feature';
-export { default as Navbar } from './navbar/navbar';
+export { default as Article } from './Article/article';
+export { default as Brand } from './brand/Brand';
+export { default as CTA } from './Cta/cta';
+export { default as Feature } from './feature/Feature';
+export { default as Navbar } from './navbar/Navbar';
